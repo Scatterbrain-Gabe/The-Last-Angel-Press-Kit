c44e1c3e9d1de8c7cf4840ff2911e449b6095a71
Scatterbrain-Gabe <133150969+Scatterbrain-Gabe@users.noreply.github.com>
2026-04-15 15:01:27 -0400

    Press Kit Initial Commit

---
+++ PressKit.cs
@@ -0,0 +1,110 @@
+/*  
+*
+*This is the official press kit using all of the original content
+*as seen on the steam store page
+*All of the image assets are subject to change
+*
+*We will be hiring a professional artist to redo
+*All of the store assets
+*
+*/
+#region Links
+	Steam Store page
+		https://store.steampowered.com/app/4293040/The_Last_Angel/
+	Official Trailer
+		https://drive.google.com/file/d/1_PvXR0-D_iAnoQdFkcxAimnF-v02q4rC/view?usp=drive_link
+	Raw Trailer: Trailer with no titles
+		https://drive.google.com/file/d/1lwgBziUf02YZ58MmrJyTrfzTsMaYS2Iu/view?usp=drive_link
+	
+#endregion
+
+#region Features
+	-Online Play: Host and play with your friends
+	-Action packed combat
+	-19 unique bosses to fight
+	-Over 76 unique weapons to choose from
+	-5 Unique characters to play as
+	-Saved data & Achievements
+	-Large open worlds
+	-4 different vehicles
+	-Playtest Available
+	
+#endregion
+#region SteamPageDescription
+About This Game
+
+
+YOU ARE THE LAST ANGEL
+
+Summoned to another World, Angels are the greatest power against an infestation of Arcane Entities.
+
+Blessed with Eternal Life, Angels are far more brave and reckless than their mortal counterparts.
+
+Stage your meteoric rise to the stars as you reclaim the godly artifacts that transform ordinary beings
+
+into cosmic horrors.
+
+
+ 
+
+PVP ENABLED
+
+No such mechanism has been designed by the higher powers to prevent the Angels from destroying one another.
+
+Angels must work together and position properly to vanquish foes; otherwise, they risk vanquishing one another.
+
+Furthermore, there is no mechanism to prevent Angels from injuring themselves with their magic. Great care must
+
+be taken when deploying powerful magic.
+
+
+
+WEAPONS GALORE
+
+There are over 50 known weapons in the Worlds below . . . for now. Without backpacks, Angels can only carry 2
+
+ranged weapons, a melee weapon, and a grenade. With these limits, Angels must balance their equipment between
+
+Offense, Defense, and Healing. Each weapon has a primary and secondary fire that do not share a cooldown. Create
+
+awesome combinations of spells to destroy your foes.
+
+
+
+EXPLORE
+
+The Days and the Nights are not the same. There is peace on the waters during the day. Night brings Sharks and
+
+Chaos. Each area houses unique weapons, chests and bosses. Talk to the residents to gain information about the
+
+surroundings. 
+
+
+
+TEST YOUR WEAPONS AT THE RANGE
+
+There is a hidden range below Purgatory that only Angels can access. Angels can measure the effectiveness of their
+
+weapons here. The higher powers have stationed an undying Crystal Entity here that never fails to make an
+
+appearance.
+
+
+
+TANKS
+
+The worlds of men have engineered fantastic machines called 'Tanks' to make up for their mortal shortcomings. These
+
+'Tanks' have wonderful durability and excellent firepower. The only difficulty is finding a way to transport the 
+
+heavy beasts.
+
+
+
+SHARKS ARE NOT YOUR FRIENDS
+
+The waters are quite hostile. Great preparation must be taken to survive at depth. Sharks are nocturnal, so if
+
+you are planning a diving expedition, go during the day.
+
+#endregion
---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+Open the PressKit.cs file using either
+	notepad++
+	Microsoft visual studio
+	
+The file will be separated into regions for easy readability
+
+The file can be opened and viewed with regular notepad, but thats no fun :D
